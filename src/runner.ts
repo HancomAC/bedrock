47b6bc45209f9eb2951d238061f13113bb4f635c
Seohyun Lee <studioSeoRii@gmail.com>
2022-07-10 16:38:46 +0900

    fix: fix script base path

--- src/runner.ts
+++ src/runner.ts
@@ -1,7 +1,7 @@
 import * as childProcess from 'child_process';
 import * as path from "path";
 
-const packageJson = require(path.join(require.main.path, 'package.json'));
+const packageJson = require(path.join(process.cwd(), 'package.json'));
 
 const makeAllPackagesExternalPlugin = {
     name: 'make-all-packages-external',
@@ -16,15 +16,15 @@ const args = require('args-parser')(process.argv);
 let builded;
 
 require('esbuild').build({
-    entryPoints: [path.join(require.main.path, args.entry)],
-    outfile: path.join(require.main.path, args.dist),
+    entryPoints: [path.join(process.cwd(), args.entry)],
+    outfile: path.join(process.cwd(), args.dist),
     bundle: true,
     plugins: [makeAllPackagesExternalPlugin],
     platform: 'node',
     define: {
         'config.version': `"${packageJson.version}"`,
-        'config.commitHash': `"${childProcess.execSync('git rev-parse HEAD', {cwd: require.main.path}).toString().trim()}"`,
-        'config.commitCount': `${childProcess.execSync('git rev-list --count HEAD', {cwd: require.main.path}).toString().trim()}`,
+        'config.commitHash': `"${childProcess.execSync('git rev-parse HEAD', {cwd: process.cwd()}).toString().trim()}"`,
+        'config.commitCount': `${childProcess.execSync('git rev-list --count HEAD', {cwd: process.cwd()}).toString().trim()}`,
         'config.buildDate': `"${new Date().toISOString()}"`,
         'config.port': args.dev ? '3006' : '80',
         'config.dev': `${args.dev}`,
@@ -39,7 +39,7 @@ require('esbuild').build({
                     console.log('✔ Build successful.')
                     console.log('⚡ Restarting server...')
                     if (builded) builded.kill();
-                    builded = childProcess.spawn('node', [path.join(require.main.path, args.dist)], {stdio: 'inherit'});
+                    builded = childProcess.spawn('node', [path.join(process.cwd(), args.dist)], {stdio: 'inherit'});
                 }
             },
         }
@@ -49,7 +49,7 @@ require('esbuild').build({
         for (let i = 0; i < process.stdout.rows; i++) console.log('');
         process.stdout.cursorTo(0, 0);
         console.log('⚡ Starting server...')
-        builded = childProcess.spawn('node', [path.join(require.main.path, args.dist)], {stdio: 'inherit'});
+        builded = childProcess.spawn('node', [path.join(process.cwd(), args.dist)], {stdio: 'inherit'});
     } else {
         console.log('✔ Build successful.')
     }
