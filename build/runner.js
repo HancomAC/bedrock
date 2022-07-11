acaf9f788c017419b77a15185175b385fb2b8f45
Seohyun Lee <studioSeoRii@gmail.com>
2022-07-11 16:03:15 +0900

    fix: rebuild

--- build/runner.js
+++ build/runner.js
@@ -27,13 +27,13 @@ var makeAllPackagesExternalPlugin = {
 };
 var args = require("args-parser")(process.argv);
 var builded;
-console.log({
-  "config.version": `"${packageJson.version}"`,
-  "config.commitHash": `"${childProcess.execSync("git rev-parse HEAD", { cwd: process.cwd() }).toString().trim()}"`,
-  "config.commitCount": `${childProcess.execSync("git rev-list --count HEAD", { cwd: process.cwd() }).toString().trim()}`,
-  "config.buildDate": `"${new Date().toISOString()}"`,
-  "config.port": args.port || (args.dev ? "3006" : "80"),
-  "config.dev": `${args.dev}`
+var config = JSON.stringify({
+  "version": `"${packageJson.version}"`,
+  "commitHash": `"${childProcess.execSync("git rev-parse HEAD", { cwd: process.cwd() }).toString().trim()}"`,
+  "commitCount": `${childProcess.execSync("git rev-list --count HEAD", { cwd: process.cwd() }).toString().trim()}`,
+  "buildDate": `"${new Date().toISOString()}"`,
+  "port": args.port || (args.dev ? "3006" : "80"),
+  "dev": `${args.dev}`
 });
 require("esbuild").build({
   entryPoints: [path.join(process.cwd(), args.entry)],
@@ -41,14 +41,7 @@ require("esbuild").build({
   bundle: true,
   plugins: [makeAllPackagesExternalPlugin],
   platform: "node",
-  define: {
-    "config.version": `"${packageJson.version}"`,
-    "config.commitHash": `"${childProcess.execSync("git rev-parse HEAD", { cwd: process.cwd() }).toString().trim()}"`,
-    "config.commitCount": `${childProcess.execSync("git rev-list --count HEAD", { cwd: process.cwd() }).toString().trim()}`,
-    "config.buildDate": `"${new Date().toISOString()}"`,
-    "config.port": args.port || (args.dev ? "3006" : "80"),
-    "config.dev": `${args.dev}`
-  },
+  define: { config },
   ...args.dev ? {
     watch: {
       onRebuild(error) {
