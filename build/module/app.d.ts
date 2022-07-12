242f01e187993d677dc07e57f48b2b9087a03d57
Seohyun Lee <studioSeoRii@gmail.com>
2022-07-12 21:12:44 +0900

    feat: add type definition for app

--- build/module/app.d.ts
+++ build/module/app.d.ts
@@ -1,7 +1,22 @@
+import express from 'express';
+import Resp from "./types/response";
 import './util/env';
-export default function ({ port, name, cb, config }?: {
-    port: number;
+declare type App = express.Application;
+declare type Handler = (req: express.Request) => Resp<any>;
+declare type HandlerRegistrator = (path: string, handler: Handler) => void;
+interface AppConfig {
+    app: App;
+    config: any;
+    get: HandlerRegistrator;
+    post: HandlerRegistrator;
+    put: HandlerRegistrator;
+    delete: HandlerRegistrator;
+    use: HandlerRegistrator;
+}
+export default function ({ port, name, cb, config }: {
+    port?: number;
     name: string;
     config: any;
-    cb: ({}: any) => Promise<void>;
+    cb?: (data: AppConfig) => null;
 }): Promise<void>;
+export {};
