4a5022d4003b563d2ae6ca25d5440016d3f9a2e8
Seohyun Lee <studioSeoRii@gmail.com>
2022-07-13 13:19:20 +0900

    Sync

--- build/module/index.js
+++ build/module/index.js
@@ -3,9 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = void 0;
+exports.router = exports.log = void 0;
 const app_1 = __importDefault(require("./app"));
 const log_1 = __importDefault(require("./util/log"));
+const router_1 = __importDefault(require("./util/router"));
 exports.log = log_1.default;
+exports.router = router_1.default;
 exports.default = app_1.default;
 //# sourceMappingURL=index.js.map
---
+++ src/module/util/router.ts
@@ -0,0 +1,35 @@
+import express, {RequestHandler, RouterOptions} from "express";
+import Resp from "../types/response";
+import handler from "./handler";
+
+type BHandler = (path: string, f: (req: express.Request) => Resp<any>) => any;
+type BRouter = express.Router & {
+    _use: RequestHandler, _get: RequestHandler, _post: RequestHandler, _delete: RequestHandler, _put: RequestHandler,
+    use: BHandler, get: BHandler, post: BHandler, delete: BHandler, put: BHandler
+}
+
+export default function (options?: RouterOptions): BRouter {
+    const r = express.Router(options) as express.Router
+        & { _use: any, _get: any, _post: any, _delete: any, _put: any, use: any, get: any, post: any, delete: any, put: any };
+    r._use = r.use;
+    r._get = r.get;
+    r._post = r.post;
+    r._delete = r.delete;
+    r._put = r.put;
+    r.use = (path: string, f: (req: express.Request) => Resp<any>) => {
+        r._use(path, handler(f))
+    }
+    r.get = (path: string, f: (req: express.Request) => Resp<any>) => {
+        r._get(path, handler(f))
+    }
+    r.post = (path: string, f: (req: express.Request) => Resp<any>) => {
+        r._post(path, handler(f))
+    }
+    r.delete = (path: string, f: (req: express.Request) => Resp<any>) => {
+        r._delete(path, handler(f))
+    }
+    r.put = (path: string, f: (req: express.Request) => Resp<any>) => {
+        r._put(path, handler(f))
+    }
+    return r;
+}
