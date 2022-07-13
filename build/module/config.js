9ae8220c34780af151cd5dc746c37e444c73f1c5
Seohyun Lee <studioSeoRii@gmail.com>
2022-07-13 16:31:16 +0900

    Sync

--- build/module/config.js
+++ build/module/config.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = { "version": "1.1.1", "commitHash": "c3f2d813469be674447c49d2f6cb129f3efe0734", "commitCount": 36, "buildDate": "2022-07-13T07:27:46.778Z", "port": 80 };
+exports.default = { "version": "1.1.2", "commitHash": "5fd60b6a443d748f3fda5cb394002d865744798f", "commitCount": 37, "buildDate": "2022-07-13T07:31:04.790Z", "port": 80 };
 //# sourceMappingURL=config.js.map
