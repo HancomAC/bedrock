3af8afdbbf3c051ae0c33be9f9c7b822d6989aa1
Seohyun Lee <studioSeoRii@gmail.com>
2022-07-14 17:20:54 +0900

    chore: rebuild

--- src/module/config.ts
+++ src/module/config.ts
@@ -1,1 +1,1 @@
-export default {"version":"1.1.14","commitHash":"70e6c7364589c93ee8dc3d979ec9a7113db47541","commitCount":49,"buildDate":"2022-07-14T08:20:07.981Z","port":80}
+export default {"version":"1.1.15","commitHash":"7f7782bd38372f5ef32344d797690cf4b5989a44","commitCount":50,"buildDate":"2022-07-14T08:20:42.521Z","port":80}
