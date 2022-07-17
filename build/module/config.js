0c9aa3b4981f04a8c835d6a62ea5abb2b8994f27
Seohyun Lee <studioSeoRii@gmail.com>
2022-07-17 16:48:08 +0900

    chore: rebuild

--- build/module/config.js
+++ build/module/config.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = { "version": "1.5.4", "commitHash": "e8fd8329c87290c2e0efb581225ac7789110b7ff", "commitCount": 74, "buildDate": "2022-07-17T07:38:59.498Z", "port": 80 };
+exports.default = { "version": "1.5.5", "commitHash": "c0b218e923def1356ff96072cd7620e687768bed", "commitCount": 76, "buildDate": "2022-07-17T07:48:00.991Z", "port": 80 };
 //# sourceMappingURL=config.js.map
--- build/module/express/handler.d.ts
+++ build/module/express/handler.d.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { ACLHandler, Handler } from "../types/router";
 export default function (...f: Handler[]): express.RequestHandler;
-export declare function acl(aclChecker?: ACLHandler, handler?: Handler): Handler;
+export declare function acl(aclChecker?: ACLHandler, handler?: Handler, checkDefault?: boolean): Handler;
 export declare function generator(f: Handler | express.RequestHandler): {
     (req: any, res: any, next: any): Promise<any>;
     refresh(): Promise<boolean>;
