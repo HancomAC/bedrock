ac7c7693ac23c9c1dc1af9134fdc3e807102fffe
Seohyun Lee <studioSeoRii@gmail.com>
2022-07-29 16:29:13 +0900

    feat: add atomic api

--- src/module/gcp/datastore.ts
+++ src/module/gcp/datastore.ts
@@ -6,5 +6,11 @@ const key = client.key.bind(client);
 const query = client.createQuery.bind(client);
 const save = client.save.bind(client);
 const get = client.get.bind(client);
+const atomic = async (cb: any) => {
+    const transaction = client.transaction();
+    await transaction.run();
+    await cb(transaction);
+    await transaction.commit();
+}
 
-export default {client, key, query, save, get};
+export default {client, key, query, save, get, atomic};
